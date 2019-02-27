9aec1ae1d8363f0c6bf69d6a1da1f3d38243a9a7
opet <opet@Academico.edu>
2019-02-26 22:05:52 -0300

    Ola mundo

---
+++ src/OlaMundo.java
@@ -0,0 +1,10 @@
+/*
+ * Primeira Classe da matéria de Linguagem de programação
+ */
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Ola Mundo!!");
+	}
+
+}
