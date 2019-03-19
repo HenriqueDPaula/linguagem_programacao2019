46cf8eb86576b8e42d4f5d2f4e0e1bef5c013d54
Henrique de Paula <henrique.depaula@hotmail.com>
2019-03-19 17:56:07 -0300

    12/03

--- src/OlaMundo.java
+++ src/OlaMundo.java
@@ -1,10 +1,12 @@
 /*
- * Primeira Classe da matéria de Linguagem de programação
+ * Primeira Classe da matéria de Linguagem de programação - 26/02/2019
+ * 
+ * @author henrique.leite
  */
 public class OlaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Ola Mundo!!");
+		System.out.println("Ola Mundo!!"); 
 	}
 
 }
---
+++ src/Operadores.java
@@ -0,0 +1,23 @@
+/*
+ * Segunda Classe da matéria de linguagem de programação - 12/03/2019
+ * 
+ * @author henrique.leite
+ */
+
+public class Operadores {
+
+	public static void main(String[] args) {
+
+		/* Comentario em bloco */
+		// Comentario em linha
+
+		int a = 4, b = 6, c = 3;
+
+		System.out.println("a + b: " + (a + b));
+		System.out.println("a + c * b: " + (a + c * b));
+		System.out.println("c * c: " + (c * c));
+		System.out.println("b / c: " + (b / c));
+		System.out.println("b % c: " + (b % c));
+		System.out.println("(a + b) * (a - b): " + ((a + b) * (a - b)));
+	}
+}
