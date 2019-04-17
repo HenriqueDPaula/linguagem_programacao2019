e29c852e6d911cb9970369e6e5015e4189984b8a
opet <opet@Academico.edu>
2019-04-16 22:07:55 -0300

    [EXE] Exercicio Fibonacci

---
+++ src/exercicios/Fibonacci.java
@@ -0,0 +1,29 @@
+package exercicios;
+
+public class Fibonacci {
+
+	static long fibo(int n) {
+		int F = 0; // atual
+		int ant = 0; // anterior
+
+		for (int i = 1; i <= n; i++) {
+
+			if (i == 1) {
+				F = 1;
+				ant = 0;
+			} else {
+				F += ant;
+				ant = F - ant;
+			}
+		}
+		return F;
+	}
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			System.out.print(Fibonacci.fibo(i) + " - ");
+		}
+
+	}
+
+}
---
+++ src/exercicios/IMC.java
@@ -0,0 +1,14 @@
+package exercicios;
+
+import util.Reader;
+
+public class IMC {
+	
+	public static void main (String[] args) {
+		double peso = Reader.readDouble("Insira seu peso: ");
+		double altura = Reader.readDouble("Insira sua altura: ");
+		
+		
+	}
+
+}
